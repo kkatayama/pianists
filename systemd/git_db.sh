59d97f578a886237c4babd8ffc648ac8d7e2924f
kkatayama <katayama@udel.edu>
2022-04-29 00:53:39 -0400

    add system daemon

--- systemd/git_db.sh
+++ systemd/git_db.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-inotifywait -q -m -e CLOSE_WRITE --format="git commit -m 'auto commit db' %w && git push origin main" ../pianists.db | bash
+# inotifywait -q -m -e CLOSE_WRITE --format="git commit -m 'auto commit db' %w && git push origin main" ../pianists.db | bash
+
+inotifywait -q -r -m -e CLOSE_WRITE --format="git commit -m 'auto commit db' %w && git push origin main" $HOME/Documents/pianists | bash
