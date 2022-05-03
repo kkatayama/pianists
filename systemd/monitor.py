7ad62539228deb45031d0920b94932d8a7c2c2d8
kkatayama <katayama@udel.edu>
2022-05-03 04:31:36 -0400

    re-organize

--- systemd/monitor.py
+++ systemd/monitor.py
@@ -1,39 +1,42 @@
 from watchdog.events import PatternMatchingEventHandler
 from watchdog.observers import Observer
 from pathlib import Path
+
+from paramiko import SSHClient
+from scp import SCPClient
 # from teddy import getLogger
 # from rich import print
 import logging
+import shutil
 import time
 import sys
 
 
+host = {
+    "ip": "192.168.1.95",
+    "port": "2222",
+    "username": "katayama",
+    "remote_path": "/Users/katayama/temp/incoming"
+}
+
+def progress4(filename, size, sent, peername):
+    if isinstance(filename, bytes):
+        filename = filename.decode()
+    sys.stdout.write("(%s:%s) %s\'s progress: %.2f%%   \r" % (peername[0], peername[1], filename, float(sent)/float(size)*100))
+
 class MonitorChanges(PatternMatchingEventHandler):
-    # def catch_all_handler(self, event):
-    #     pass
-    #     log.debug(event)
-
-    # def on_moved(self, event):
-    #     # if ".db" in event.src_path:
-    #     #     log.warn(f'DB Moved???: "Updating GitHub"')
-    #     #     # self.git_update()
-    #     return
-
-    # def on_created(self, event):
-    #     if ".pdf" in event.src_path:
-    #         logging.info(f'PDF Item Created: "SCP to Server"')
-    #         logging.info(event)
-
-    # def on_deleted(self, event):
-    #     # if ".db" in event.src_path:
-    #     #     log.info(f'DB Item Deleted: "Updating GitHub"')
-    #     #     # self.git_update()
-    #     return
-
     def on_modified(self, event):
         if ".pdf" in event.src_path:
+            pdf_file = Path(event.src_path)
             logging.info(f'PDF Item Modified: "SCP to Server"')
             logging.info(event)
+            time.sleep(4)
+
+            with SSHClient() as ssh:
+                ssh.load_system_host_keys()
+                ssh.connect(hostname=host['ip'], port=host['port'], username=host['username'])
+                with SCPClient(ssh.get_transport(), progress4=progress4) as scp:
+                    scp.put(files=str(pdf_file), remote_path=host['remote_path'], recursive=False)
 
 
 if __name__ == '__main__':
