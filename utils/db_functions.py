2bd9ae2ba0e16f63f2e34eb44b3d706ca75c71a4
kkatayama <katayama@udel.edu>
2022-05-03 03:18:11 -0400

    db_sync

--- utils/db_functions.py
+++ utils/db_functions.py
@@ -445,7 +445,7 @@ def checkPassword(plaintext, hex_pass):
     return False
 
 def git_update():
-    time.sleep(1)
+    # time.sleep(1)
     # cmds = (
     #     ["git", "add", "--all"],
     #     ["git", "commit", "--author=katayama@udel.edu", "-am", "sync db changes"],
@@ -458,7 +458,7 @@ def git_update():
     )
     for cmd in cmds:
         print(subprocess.Popen(cmd, cwd=Path.cwd().as_posix(), universal_newlines=False, shell=None))
-        time.sleep(1)
+        # time.sleep(1)
         # print(' '.join(cmd))
         # out = subprocess.run(' '.join(cmd), cwd=Path.cwd().as_posix(), shell=True, capture_output=True, text=True).stdout.strip()
         # print(out)
