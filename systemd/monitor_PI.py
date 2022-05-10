13e5b8a84b9f122723e9111e6a494e53f52c8778
kkatayama <katayama@udel.edu>
2022-05-10 09:40:09 -0400

    watchdog server workflow

--- systemd/monitor_PI.py
+++ systemd/monitor_PI.py
@@ -98,8 +98,8 @@ class MonitorChanges(PatternMatchingEventHandler):
             # -- 4. cleanup
             shutil.rmtree(str(TEMP_PATH))
             TEMP_PATH.mkdir(exist_ok=True)
-            shutil.rmtree(str(WATCH_PATH))
-            WATCH_PATH.mkdir(exist_ok=True)
+            #shutil.rmtree(str(WATCH_PATH))
+            #WATCH_PATH.mkdir(exist_ok=True)
 
 
 if __name__ == '__main__':
