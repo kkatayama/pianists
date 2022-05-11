3353ede0d3f455577e8af921478a0b6a4cdca9cf
kkatayama <katayama@udel.edu>
2022-05-11 18:44:12 -0400

    watchdog server workflow

--- utils/parse_mxl.py
+++ utils/parse_mxl.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from pathlib import Path
-import pandas as pd
+# import pandas as pd
 import argparse
 import muspy
+import json
 
 
 if __name__ == '__main__':
@@ -17,11 +18,35 @@ if __name__ == '__main__':
     if args.base_path:
         BASE_PATH = Path(args.base_path)
 
-    music = muspy.read_musicxml(str(MXL_FILE))
-    data = music.to_ordered_dict()
+    # data = music.to_ordered_dict()
+    music = muspy.read_musicxml(str(MXL_FILE), resolution=1)
+    data = json.loads(json.dumps(music.to_ordered_dict()))
 
-    df = pd.DataFrame([dict(i) for i in data["tracks"][0]["notes"]])
-    df.to_csv(f'{BASE_PATH.joinpath(MXL_FILE.stem)}.csv', index=False)
+    # -- split notes by treble and bass clef
+    treble = list(filter(lambda x: x["pitch"] >= 60, data["tracks"][0]["notes"]))
+    bass = list(filter(lambda x: x["pitch"] < 60, data["tracks"][0]["notes"]))
+
+    # -- generate p_code -- #
+    key_map = {32: 1, 16: 1, 12: 1.33, 8: 2, 6: 2.66, 4: 4, 3: 5.33, 2: 8, 1: 16}
+    qpm = data["tempos"][0]["qpm"] if data.get("tempos") else 100
+    num = data["time_signatures"][0]["numerator"] if data.get("time_signatures") else 4
+    den = data["time_signatures"][0]["denominator"] if data.get("time_signatures") else 4
+    p_code_treble = ["R", f"{qpm}", f"{num}/{den}"]
+
+    # -- format each note -- #
+    for note in treble:
+        key = note["pitch_str"].replace("#", "S")
+        if "b" in key:
+            key = key.replace(key[0], chr(ord(key[0]) - 1)).replace("b", "S")
+        rate = key_map[note["duration"]]
+        p_code_treble.append(f"{rate}/{key}")
+
+    # -- export p-code --#
+    with open("music.pcode", "w") as f:
+        f.write(" ".join(p_code_treble) + " ")
+
+    # df = pd.DataFrame([dict(i) for i in data["tracks"][0]["notes"]])
+    # df.to_csv(f'{BASE_PATH.joinpath(MXL_FILE.stem)}.csv', index=False)
 
     with open(f'{BASE_PATH.joinpath(MXL_FILE.stem)}.json', 'w') as f:
         music.save_json(f)
